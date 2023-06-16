02976d864a339f5c2614aefc542ca3bd8f18ff03
颜贵彬 <yanguibin@aomiapp.com>
2023-06-16 17:46:56 +0800

    wip

--- build/gen.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-antlr4 -Dlanguage=JavaScript ./lib/HTMLLexer.g4
-antlr4 -Dlanguage=JavaScript ./lib/HTMLParser.g4
--- public/test.vue
+++ public/test.vue
@@ -1,3 +1,36 @@
 <template>
-  <div v-for="item in list" click.stop="@onClick(item)" data-nani="1">{{item}}</div>
+  <div v-for="item in list" @click.stop="onClick(item)" data-nani="1">{{item}}</div>
 </template>
+
+
+<script>
+console.log('yes')
+</script>
+
+
+<style lang="scss" scoped>
+.page-wrapper {
+  padding-bottom: 106px;
+  @include supports-safe-area-inset-bottom(padding-bottom, 106);
+  // padding-top: 12px;
+  background-color: #f5f5f5;
+  min-height: 100%;
+}
+
+.bg-wrap {
+  background-color: white;
+  padding-top: 12px;
+}
+
+.swiper-wrap {
+  display: block;
+  margin: 0 auto;
+  width: 351px;
+  height: 131px;
+  overflow: hidden;
+
+  .img {
+    border-radius: 12px;
+  }
+}
+</style>
